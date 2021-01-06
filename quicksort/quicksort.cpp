9fd87aa71bb3a0a4ecc3088837269115f82854b2
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2021-01-05 21:36:53 -0600

    off by one error was causing problems with sort

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -1,9 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 #include "quicksort.hpp"
 
+void test() {
+    for (size_t niter = 0; niter < 1000; niter++) {
+        std::vector<int> v;
+        for (size_t i = 0; i < 10000; i++) {
+            v.push_back(rand() & 0xff);
+        }
+        auto copy = v;
+        quicksort(v);
+        std::sort(copy.begin(), copy.end());
+        if (copy != v) {
+            std::cerr << "fail" << std::endl;
+            print_vec(v);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
 int main() {
+    srand(42);
     std::vector<int> v;
     print_vec(v);
 
@@ -22,5 +42,6 @@ int main() {
     quicksort(v2);
     print_vec(v2);
 
+    test();
     std::cout << "done" << std::endl;
 }
--- quicksort/quicksort.hpp
+++ quicksort/quicksort.hpp
@@ -11,7 +11,7 @@ void print_vec(const std::vector<T> &v) {
         for (size_t i = 0; i < (v.size() - 1); i++) {
             std::cout << v[i] << ", ";
         }
-        std::cout << v[v.size()];
+        std::cout << v[v.size() - 1];
     }
     std::cout << "]\n";
 }
@@ -19,24 +19,38 @@ void print_vec(const std::vector<T> &v) {
 template <typename T>
 static size_t partition(std::vector<T> &v, size_t lo, size_t hi) {
     auto pivot = v[hi];
-    auto pivot_index = lo;
-    size_t i = 0;
-    for (i = lo; i < hi; i++) {
-        if (v[i] <= pivot) {
-            std::swap(v[i], v[pivot_index]);
-            pivot_index++;
+    auto i = lo;
+    for (auto j = lo; j < hi; j++) {
+        if (v[j] <= pivot) {
+            std::swap(v[i], v[j]);
+            i++;
         }
     }
-    std::swap(v[pivot_index], v[hi]);
+    std::swap(v[i], v[hi]);
     return i;
+}
+
+template <typename T>
+static void insertionsort(std::vector<T> &v, size_t lo, size_t hi) {
+    for (size_t i = lo; i < hi + 1; i++) {
+        size_t j = i;
+        while ((j > 0) && (v[j-1] > v[j])) {
+            std::swap(v[j], v[j-1]);
+            j--;
+        }
+    }
 }
 
 template <typename T>
 static void rec_quicksort(std::vector<T> &v, size_t lo, size_t hi) {
     if (lo < hi) {
-        size_t p = partition(v, lo, hi);
-        rec_quicksort(v, lo, p - 1);
-        rec_quicksort(v, p + 1, hi);
+        if ((hi - lo) < 56) {
+            insertionsort(v, lo, hi);
+        } else {
+            size_t p = partition(v, lo, hi);
+            rec_quicksort(v, lo, p - 1);
+            rec_quicksort(v, p + 1, hi);
+        }
     }
 }
 
